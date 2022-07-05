79f80ba733776c0466baa0063a32c923daaf29fa
SeanGrant18 <sgseangrant@gmail.com>
2022-07-05 12:09:18 +0000

    Questions

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        prompt: "How long was avengers endgame?\n(a) 3h 2m\n\(b) 4h\n(c) 2h 30m",
+        answer: "a"
+    },
+    {
+        prompt: "How long was Terminator?\n(a) 1h 2m\n\(b) 1h 47m\n(c) 3h 30m",
+        answer: "b"
+    },
+    {
+        prompt: "How many seasons in Prison Break?\n(a) 5\n\(b) 4\n(c) 7",
+        answer: "a"
+    },
+]
