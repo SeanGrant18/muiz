50dcda6ef608cb5d4960bc94e39322564bd8a1cc
SeanGrant18 <sgseangrant@gmail.com>
2022-07-06 11:21:33 +0000

    Added multiple functuins

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
+// Displaying The First Block Of Question.
 document.getElementsByClassName('container')[0].style.display = "block";
+
+// Defining The Next Function.
+function next(id) {
+    document.getElementsByClassName('container')[id-1].style.display = "none"
+    document.getElementsByClassName('container')[id].style.display = "block"
+}
+
+// Getting Final Result.
+function result() {
+    var score = 0;
+    if (document.getElementById('correct1').checked) {
+        score++;
+    }
+    if (document.getElementById('correct2').checked) {
+        score++;
+    }
+    if (document.getElementById('correct3').checked) {
+        score++;
+    }
+    alert("You Have A Total Score Of : "+ score);
+}
